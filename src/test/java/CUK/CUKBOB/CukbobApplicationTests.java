50bf898bb80ad3d3279667abfd520e9e5a7b1f61
mingMINJOO <smj8223@naver.com>
2025-03-28 00:42:10 +0900

    [setting/#1] 프로젝트 세팅

---
+++ src/test/java/CUK/CUKBOB/CukbobApplicationTests.java
@@ -0,0 +1,13 @@
+package CUK.CUKBOB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CukbobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
