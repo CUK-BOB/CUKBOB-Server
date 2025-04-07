125f04c0a3d1d0b032001b9f1d41144ac56c67ce
mingMINJOO <smj8223@naver.com>
2025-04-07 17:09:06 +0900

    [refactor/#11] 닉네임 예외처리

--- src/main/java/CUK/CUKBOB/oauth/Service/UserService.java
+++ src/main/java/CUK/CUKBOB/oauth/Service/UserService.java
@@ -1,6 +1,5 @@
 package CUK.CUKBOB.oauth.Service;
 
-import CUK.CUKBOB.oauth.Domain.SocialType;
 import CUK.CUKBOB.oauth.Domain.User;
 import CUK.CUKBOB.oauth.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +14,15 @@ public class UserService {
         User user = userRepository.findById(userId) //유저레포지토리에서 JpaRepository를 상속받고 있어서 자동으로 findById 제공!
                 .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
 
+        //사용자의 닉네임이 이미 있을 때
         if (user.getNickname() != null) {
             throw new IllegalStateException("이미 닉네임이 설정된 사용자입니다.");
+        }
+
+        //다른 유저가 사용중인 닉네임일때
+        boolean nicknameExists = userRepository.existByNickname(nickname);
+        if (nicknameExists) {
+            throw new IllegalArgumentException("이미 사용중인 닉네임입니다.");
         }
 
         user.setNickname(nickname);
