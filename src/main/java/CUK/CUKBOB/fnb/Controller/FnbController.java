23842209a459829b779d35fb9090ae4d8890637d
mingMINJOO <smj8223@naver.com>
2025-04-09 00:32:38 +0900

    [fix/#13] Security 인증문제해결

--- src/main/java/CUK/CUKBOB/fnb/Controller/FnbController.java
+++ src/main/java/CUK/CUKBOB/fnb/Controller/FnbController.java
@@ -6,16 +6,12 @@ import CUK.CUKBOB.oauth.Dto.Response.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
