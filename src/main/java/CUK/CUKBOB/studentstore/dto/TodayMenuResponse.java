3e171ff64f8c602548bbf562ad145d0125a3ddff
TaeRim0701 <xofla71@naver.com>
2025-05-04 00:32:38 +0900

    [fix/#18] 오늘학식조회, 주간학식조회 조,중,석식 시간 세분화

---
+++ src/main/java/CUK/CUKBOB/studentstore/dto/TodayMenuResponse.java
@@ -0,0 +1,35 @@
+package CUK.CUKBOB.studentstore.dto;
+
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuResponse {
+    private MealGroup morning;
+    private MealGroup lunch;
+    private MealGroup dinner;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MealGroup {
+        private List<RestaurantMenu> restaurant;
+    }
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class RestaurantMenu {
+        private Long restaurant_id;
+        private String restaurant_name;
+        private List<String> menu;
+        private List<Integer> price;
+        private String restaurant_time;
+    }
+}
